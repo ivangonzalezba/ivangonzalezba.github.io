573b05d1dd2fcccfdd406022aad5ca4297aa5f84
Ivan <ivangonzalezba@gmail.com>
2020-09-04 15:26:28 -0300

    darkmode 1.0

--- js/jsDarkMode.js
+++ js/jsDarkMode.js
@@ -1,28 +1,28 @@
 /*obtener el link style*/
-var cssDarkMode = document.getElementsByName('cssDarkMode');
+var cssDarkMode = document.querySelector('.cssDarkMode');
+/*obtener el input darkmode*/
+var botonDarkMode = document.querySelector('.contenedorDarkMode__input');
 /*obtener el valor de darkmode de localstorage*/
 if (localStorage.getItem('Iag_Web_DarkMode')) {
     let darkmode = localStorage.getItem('Iag_Web_DarkMode');
     if (darkmode == 'true') {
-        cssDarkMode[0].setAttribute("href", "css/styleDarkMode.css");
+        cssDarkMode.setAttribute("href", "css/styleDarkMode.css");
+        botonDarkMode.setAttribute("value", "1");
     } else {
-        cssDarkMode[0].setAttribute("href", "");
+        cssDarkMode.setAttribute("href", "");
+        botonDarkMode.setAttribute("value", "0");
     }
 } else {
     localStorage.setItem('Iag_Web_DarkMode', 'false');
 }
-/*agregar listener solo si estoy en index*/
-if (document.getElementById('index')) {
-    var botonDarkMode = document.getElementById('btnDarkMode');
 
-    botonDarkMode.addEventListener('click', function () {
-        let darkmode = localStorage.getItem("Iag_Web_DarkMode");
-        if (darkmode == 'false') {
-            cssDarkMode[0].setAttribute("href", "css/styleDarkMode.css");
-            localStorage.setItem('Iag_Web_DarkMode', 'true');
-        } else {
-            cssDarkMode[0].setAttribute("href", "");
-            localStorage.setItem('Iag_Web_DarkMode', 'false');
-        }
-    });
-}
+botonDarkMode.addEventListener('change', function () {
+    let darkmode = localStorage.getItem("Iag_Web_DarkMode");
+    if (darkmode == 'false') {
+        cssDarkMode.setAttribute("href", "css/styleDarkMode.css");
+        localStorage.setItem('Iag_Web_DarkMode', 'true');
+    } else {
+        cssDarkMode.setAttribute("href", "");
+        localStorage.setItem('Iag_Web_DarkMode', 'false');
+    }
+});
